3490f6d0ce3d3682bdf04fe1cc2abf5a697e5cec
Habibun Noby <habibun.noby@gmail.com>
2022-04-29 23:48:57 +0600

    feat: add geoip2

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use GeoIp2\Database\Reader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +17,39 @@ class HomepageController extends AbstractController
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
+    }
+
+    /**
+     * @Route("/city", name="app_city")
+     */
+    public function city(): Response
+    {
+        $projectRoot = $this->getParameter('kernel.project_dir');
+        // This creates the Reader object, which should be reused across
+        // lookups.
+        $reader = new Reader($projectRoot.'/assets/vendor/geolite/GeoLite2-City.mmdb');
+
+        // Replace "city" with the appropriate method for your database, e.g.,
+        // "country".
+        $record = $reader->city('103.162.209.114');
+        dd($record);
+
+        echo $record->country->isoCode."\n";
+        echo $record->country->name."\n";
+        echo $record->country->names['zh-CN']."\n";
+
+        echo $record->mostSpecificSubdivision->name."\n";
+        echo $record->mostSpecificSubdivision->isoCode."\n";
+
+        echo $record->city->name."\n";
+
+        echo $record->postal->code."\n";
+
+        echo $record->location->latitude."\n";
+        echo $record->location->longitude."\n";
+
+        echo $record->traits->network."\n";
+
+        exit();
     }
 }
